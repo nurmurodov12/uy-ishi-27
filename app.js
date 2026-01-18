3ef52a699644c5b380e4c8c4014588faf23dea06
temurx_2009 <xurshidberdiyev315@gmail.com>
2026-01-18 17:55:40 +0500

    web

---
+++ app.js
@@ -0,0 +1,136 @@
+// for masala
+// 1-masala
+// let a = Number(prompt("a son kiriting"))
+// let b = Number(prompt("b son kiriting"))
+
+// if (a < b) {
+//     for(; a <= b; a++) {
+//         console.log(a);
+
+//     }
+//     console.log("a sonida", a, "ta20 son bor");
+
+// } else {
+// console.log("iltimos a soni b dan katta bulishi kerak ");
+
+//}
+
+// 2-masala
+// let a = 10;
+// let b = 5;
+
+// if (a > b) {
+//     for (; a > b; a--) {
+//         console.log(a);
+
+//     }
+//     console.log("bu yerda", a , "ta son chiqdi");
+
+// }
+
+// 3-masala
+// let narxi = 10
+// let kg = 1
+// for (; 10 >= kg; kg++) {
+//     console.log(narxi * kg, "so'm");
+
+// }
+
+// 4-masala
+// let narxi = 10
+// let kg = 11
+// for (; kg < 20; kg += 1 ) {
+//     let butun = kg / 10
+//     console.log(butun * narxi, "som");
+
+// }
+// 5-masala
+// let a = 1
+// let b = 10
+// let count = 0
+// for (; a <= b; a++) {
+//     count += a
+
+// }
+// console.log(count);
+
+// 6-masala
+// let a = 1
+// let b = 10
+// let count = a
+// for (; a <= b; a++) {
+//     count *= a
+
+// }
+
+// console.log(count);
+
+// 7-masala
+// let a = 10
+// let b = 20
+// let yigindi = 0
+// for (; a <= b; a++) {
+//    yigindi += a*a
+
+// }
+
+// console.log(yigindi);
+
+// 8-masala
+// let n = 4
+// let s = 0
+// for (let i = 1; i <= n; i++) {
+//     s += 1 / i 
+    
+// }
+// console.log(s);
+
+// 9-masala
+// let n = 5
+// let s = 1
+// for (let i = 1.1; i <= n; i += 0.1) {
+//     s *= i
+// }
+// console.log(s);
+
+// 15-masala
+// let n = 12
+// let k = 4
+// let s = 0
+// for (let i = 1; i <= n; i++) {
+//     s += i ** k
+// }
+// console.log(s);
+
+
+// 16-masala
+// let n = 3;
+// let s = 0;
+// for (let i = 1; i <= n; i++) {
+//     s += i**i
+    
+// }
+// console.log(s);
+
+
+// while loop masala
+
+// for || while || do while
+// let count = 0
+// while (count < 10) {
+//     count += 0.1
+//     console.log("lopping", count.toFixed(1))
+// }
+
+
+// do while
+// let son = 10;
+// let userNum;
+// do {
+//     userNum = Number(prompt("parol"))
+    
+// } while (userNum != son)
+
+const start = parseInt(prompt('n son kiriting '))
+const end = parseInt(prompt('oxirgi son kiriting'))
+
